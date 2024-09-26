dd5a3fbe60f116191f8487d0efded28fd47cb833
Matti Salimi <mattisalimi@gmail.com>
2024-09-26 11:15:58 +0200

    set default font to Roboto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{jsx,js}'],
-  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
-        roboto: ['roboto', 'serif'],
-      }
+        serif: ["Roboto", "serif"],
+      },
     },
   },
   plugins: [],

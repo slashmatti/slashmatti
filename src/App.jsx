73ba1a9ff921f0cd771deb7a60f8f4a59361c013
Matti Salimi <mattisalimi@gmail.com>
2024-09-26 11:31:07 +0200

    added component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Header from './components/Header';
 import Projects from './components/Projects';
 import Blog from './components/Blog';
 import About from './components/About';
+import Socials from './components/Socials';
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Socials />
       <Projects />
       <Blog />
       <About />
---
+++ src/components/Socials.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Socials() {
+    return (
+        <div className='flex pb-6 font-sans'>
+            Socials
+        </div>
+    )
+}
+
+export default Socials;

28b50dd020de5d99aee1c17cd2accaa1c2a878d1
Matti Salimi <mattisalimi@gmail.com>
2024-09-26 11:20:33 +0200

    added placeholder images

--- src/data/project.js
+++ src/data/project.js
@@ -2,21 +2,21 @@ export default [
     {
         title: 'BlogText',
         desc: 'Widget to receive SMS messages from users',
-        imgUrl: '',
+        imgUrl: 'src/assets/fb2.jpg',
         link: 'https://github.com/slashmatti/blogtext',
     },
 
     {
         title: 'Unidentified Feedback',
         desc: 'Get anonymous feedback via Twitter from users',
-        imgUrl: '',
+        imgUrl: 'src/assets/giggerfriskis.png',
         link: 'https://github.com/slashmatti/unidentifiedfeedback',
     },
 
     {
         title: 'Stakeholder Update Network',
         desc: 'Give and get newsletter updates from Founders',
-        imgUrl: '',
+        imgUrl: 'src/assets/migos-culture-ii.png',
         link: 'https://github.com/slashmatti/stakeholderupdates',
     },
 ]

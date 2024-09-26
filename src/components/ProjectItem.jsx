a60df7e0dff1f972d069391e3646e216ec1befdf
Matti Salimi <mattisalimi@gmail.com>
2024-09-26 11:49:36 +0200

    added links to project items

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 function ProjectItem({ title, desc, imgUrl, link }) {
     return (
         <div>
-            <img
-                src={imgUrl}
-                alt="project"
-                className='w-full h-36 md:h-48 object-cover cursor-pointer'
-            />
+            <a href={link} target="_blank">
+                <img
+                    src={imgUrl}
+                    alt="project"
+                    className='w-full h-36 md:h-48 object-cover cursor-pointer'
+                />
+            </a>
             <div className='w-full p-4'>
                 <h3>{title}</h3>
                 <p>{desc}</p>
--- src/data/project.js
+++ src/data/project.js
@@ -3,20 +3,20 @@ export default [
         title: 'BlogText',
         desc: 'Widget to receive SMS messages from users',
         imgUrl: 'src/assets/fb2.jpg',
-        link: 'https://github.com/slashmatti/blogtext',
+        link: 'https://github.com/slashmatti/blog_texts',
     },
 
     {
         title: 'Unidentified Feedback',
         desc: 'Get anonymous feedback via Twitter from users',
         imgUrl: 'src/assets/giggerfriskis.png',
-        link: 'https://github.com/slashmatti/unidentifiedfeedback',
+        link: 'https://github.com/slashmatti/unidentified_feedback',
     },
 
     {
         title: 'Stakeholder Update Network',
         desc: 'Give and get newsletter updates from Founders',
         imgUrl: 'src/assets/migos-culture-ii.png',
-        link: 'https://github.com/slashmatti/stakeholderupdates',
+        link: 'https://github.com/slashmatti/founder_digest',
     },
 ]

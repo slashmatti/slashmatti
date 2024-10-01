20fcdec56c2d5ffbb8bb712974820efa7eb79e21
Matti Salimi <mattisalimi@gmail.com>
2024-10-01 08:36:15 +0200

    added data science projects

--- src/data/project.js
+++ src/data/project.js
@@ -21,9 +21,30 @@ export default [
     },
     
     {
-        title: 'Stakeholder Update Network',
-        desc: 'Give and get newsletter updates from Founders',
-        imgUrl: 'src/assets/migos-culture-ii.png',
-        link: 'https://github.com/slashmatti/founder_digest',
+        title: 'Car Data Visualized',
+        desc: 'Shiny app that produces boxplots of the Auto dataset',
+        imgUrl: 'src/assets/ddp1.png',
+        link: 'https://github.com/slashmatti/DDP-Project',
+    },
+
+    {
+        title: 'How Many Steps?',
+        desc: 'Analysis of the raw data from a fitness tracking device',
+        imgUrl: 'src/assets/repdata.png',
+        link: 'https://slashmatti.github.io/RepData_PeerAssessment1/',
+    },
+
+    {
+        title: 'Manual Vs Automatic',
+        desc: 'Statistical analysis on fuel efficiency',
+        imgUrl: 'src/assets/regmods.png',
+        link: 'https://github.com/slashmatti/DDP-Project',
+    },
+
+    {
+        title: 'Weigh Lifting Correctly',
+        desc: 'Predictive model that classifies in what manner a bicep curl was performed',
+        imgUrl: 'src/assets/pml1.png',
+        link: 'https://slashmatti.github.io/PML-Project/',
     },
 ]

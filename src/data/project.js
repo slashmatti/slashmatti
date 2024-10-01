7a3520a339e095473b6a5a34b7134183ca92e39b
Matti Salimi <mattisalimi@gmail.com>
2024-10-01 13:53:09 +0200

    completed projects

--- src/data/project.js
+++ src/data/project.js
@@ -2,21 +2,21 @@ export default [
     {
         title: 'BlogTexts',
         desc: 'Widget for your website visitors to send SMS messages to you',
-        imgUrl: 'src/assets/fb2.jpg',
+        imgUrl: 'src/assets/bt.png',
         link: 'https://github.com/slashmatti/blog_texts',
     },
 
     {
         title: 'Unidentified Feedback',
-        desc: 'Get anonymous feedback via Twitter from users',
-        imgUrl: 'src/assets/giggerfriskis.png',
+        desc: 'Share feedback anonymously via Twitter',
+        imgUrl: 'src/assets/uf.png',
         link: 'https://github.com/slashmatti/unidentified_feedback',
     },
 
     {
-        title: 'Stakeholder Update Network',
-        desc: 'Give and get newsletter updates from Founders',
-        imgUrl: 'src/assets/migos-culture-ii.png',
+        title: 'Founder Digest',
+        desc: 'A network for giving and getting newsletter updates from Founders',
+        imgUrl: 'src/assets/fd1.png',
         link: 'https://github.com/slashmatti/founder_digest',
     },
     

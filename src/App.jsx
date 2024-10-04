9a9318deffb135b208637a8432e736620fc51f67
Matti Salimi <mattisalimi@gmail.com>
2024-10-04 12:53:43 +0200

    changed max width

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Nav from './components/Nav';
 
 function App() {
   return (
-    <div className="App px-12 py-8 space-y-3 container max-w-7xl mx-auto">
+    <div className="App px-12 py-8 space-y-3 container max-w-[1360px] mx-auto">
       <Header />
       <Nav />
       <Projects />
